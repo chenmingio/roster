ed7bc55251bb1240921bbeb7ae5ea4b5932a3b9c
ChenMing <chenming@192.168.0.100>
2019-05-10 21:40:19 +0800

    update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-from bottle import get, put, route, run, debug, request, response
+from bottle import get, post, delete, route, run, debug, request, response
 
 # record the roster situation of a department members under certain week
 
@@ -19,17 +19,55 @@ def create_table():
     ''' create table for data record'''
     conn = sqlite3.connect('roster.db')
     c = conn.cursor()
-    c.execute('''CREATE TABLE records (date DATE, name VARCHAR, location VARCHAR)''')
+    c.execute("CREATE TABLE records (date TEXT, name VARCHAR, location VARCHAR)")
     conn.commit()
     conn.close()
 
 
-@get('week/<wk>')
+def insert_record(record):
+    ''' insert a record according to given compound data '''
+    conn = sqlite3.connect('roster.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO records (date, name, location) VALUES (?,?,?)", record)
+    conn.commit()
+    conn.close()
+
+
+def delete_record(record):
+    ''' delete the record in DB'''
+    conn = sqlite3.connect('roster.db')
+    c = conn.cursor()
+    c.execute("DELETE FROM records WHERE date=? AND name=?", record)
+    conn.commit()
+    conn.close()
+
+
+@get('/week/<wk>')
 def week():
-    # URL(week/String) -> list of records
-    # produce the list of records within the given weeks
-    # 08 -> List of records which is in week08 of year 2019
+    ''' URL(week/String) -> list of records
+    produce the list of records within the given weeks
+    example: 08 -> List of records which is in week08 of year 2019'''
     pass
 
 
+@post('/people/<name>')
+def upsert(name):
+    ''' add a record into DB according to request, by trying to delete it if already exists'''
+    delete_record((request.forms.date, request.forms.name))
+    record_data = (request.forms.date, request.forms.name,
+                   request.forms.location)
+    insert_record(record_data)
+
+
+@delete('/people/<name>')
+def delete(name):
+    ''' delete a record'''
+    to_delete = (request.forms.date, request.forms.name)
+    delete_record(to_delete)
+
+
 # create_table()
+
+
+if __name__ == "__main__":
+    run(host='localhost', port=8080, debug=True, reloader=True)
