208ee621a8aaf3832dcb5cf0ab108d318a626337
ChenMing <chenming@192.168.0.100>
2019-05-10 19:39:36 +0800

    fun-create-table done

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import sqlite3
+
 from bottle import get, put, route, run, debug, request, response
 
 # record the roster situation of a department members under certain week
@@ -8,8 +9,19 @@ from bottle import get, put, route, run, debug, request, response
 # interp. the date of the record, the Name of the member and the location of him/her.
 
 
+def connect_db():
+    '''connect db=roster.db and build cursor'''
+    conn = sqlite3.connect('roster.db')
+    c = conn.cursor()
+
+
 def create_table():
-    cursor.
+    ''' create table for data record'''
+    conn = sqlite3.connect('roster.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE records (date DATE, name VARCHAR, location VARCHAR)''')
+    conn.commit()
+    conn.close()
 
 
 @get('week/<wk>')
@@ -17,3 +29,7 @@ def week():
     # URL(week/String) -> list of records
     # produce the list of records within the given weeks
     # 08 -> List of records which is in week08 of year 2019
+    pass
+
+
+# create_table()
